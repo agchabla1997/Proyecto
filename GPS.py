233c1f5adeea604c93e58370e6a7cdfd7bfdcea3
agchabla1997 <agchabla@espe.edu.ec>
2022-06-16 23:04:27 -0500

    comit

--- GPS.py
+++ GPS.py
@@ -6,6 +6,7 @@ class Grafo:
     def __init__(self, numero_nodos, locaciones, dirigido=True):
         try:
             self.m_numero_nodos = numero_nodos
+            self.m_nodos = range(self.m_numero_nodos)
             self.m_dirigido = dirigido
             self.m_locaciones = locaciones
             self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
