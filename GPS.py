e00f758f7a7b703f004a6fec475e11159f40ca4d
agchabla1997 <agchabla@espe.edu.ec>
2022-06-16 22:47:02 -0500

    Commit

--- GPS.py
+++ GPS.py
@@ -1,3 +1,14 @@
 from queue import Queue
 from sklearn import tree
 from sqlalchemy import true
+
+class Grafo: 
+    def __init__(self, numero_nodos, locaciones, dirigido=True):
+        try:
+            self.m_numero_nodos = numero_nodos
+            self.m_dirigido = dirigido
+            self.m_locaciones = locaciones
+            self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
+        except Exception as e:  
+            print(e)
+    
